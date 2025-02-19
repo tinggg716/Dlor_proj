3af7ad4ce2c9b5200a50f33a76f43868a7d5ae14
tinggg716 <chialiting716@gmail.com>
2025-02-19 22:34:45 +0800

    Create dlor_pro.py

---
+++ dlor_pro.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+with open("classifier.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
+
+st.title("sea creature")
+st.write("sea creature, crab, jellyfish, clams")
+
+sepal_length = st.slider("Sepal Length (cm)", min_value=4.0, max_value=8.0, step=0.1)
+sepal_width = st.slider("Sepal Width (cm)", min_value=2.0, max_value=5.0, step=0.1)
+petal_length = st.slider("Petal Length (cm)", min_value=1.0, max_value=7.0, step=0.1)
+petal_width = st.slider("Petal Width (cm)", min_value=0.1, max_value=2.5, step=0.1)
+
+if st.button("Predict"):
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model.predict(features)
+    st.write(f"Predicted Iris Species: {prediction[0]}")
